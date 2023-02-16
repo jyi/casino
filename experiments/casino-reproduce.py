2be0cc8a85312a6da67e57aa185560f32e821ecb
CasinoRepair <111937819+CasinoRepair@users.noreply.github.com>
2023-02-16 11:32:25 +0900

    Update casino-reproduce.py: add recoder/alpha-repair

--- experiments/casino-reproduce.py
+++ experiments/casino-reproduce.py
@@ -6,6 +6,12 @@ import d4j_tbar
 import getopt
 import seeds
 
+
+def execute_cmd(cmd: list) -> int:
+    print(" ".join(cmd))
+    result = subprocess.run(cmd)
+    return result.returncode
+
 def main(argv):
     opts,args=getopt.getopt(argv,"",['finish-correct-patch','timeout=','max-iter='])
     if len(args)<5:
@@ -24,6 +30,10 @@ def main(argv):
             max_iter=int(o)
 
     project=args[0]
+    if "-" in project:
+        proj, bid = project.split("-")
+    else:
+        proj, bid = project.split("_")
     output_dir=args[1]
     mode=args[2]
     tool=args[3]
@@ -45,35 +55,40 @@ def main(argv):
             cor_str=cor_str[:-1]
             correct_dict[elem[0]]=cor_str
 
+    default_opts = ["python3", "../casino.py", "--use-exp-alpha", "-t", "180000", "--use-pass-test", "--seed", str(seed)]
+    if timeout > 0:
+        default_opts.extend(["-T", str(timeout)])
+    if max_iter > 0:
+        default_opts.extend(["-E", str(max_iter)])
     if mode=='casino':
         if tool=='avatar' or tool=='fixminer' or tool=='kpar' or tool=='tbar':
-            cmd=['python3','../casino.py','-o',output_dir,'-m','guided',"--use-exp-alpha",
-                    '--tbar-mode','-w',f'{tool}/d4j/{project}/0' if tool=='fixminer' else f'{tool}/d4j/{project}','-t','180000','--use-pass-test']
+            project = f"{proj}_{bid}"
+            cmd= default_opts + ["-o", output_dir, "-m", "guided", "--tbar-mode",
+                    '-w',f'{tool}/d4j/{project}/0' if tool=='fixminer' else f'{tool}/d4j/{project}']
             if project in correct_dict:
                 cmd.append('-c')
                 cmd.append(correct_dict[project])
-            if seed!=-1:
-                cmd.append('--seed')
-                cmd.append(str(seed))
-            else:
-                cmd.append('--seed')
-                cmd.append('0')
             if tool=='fixminer':
                 cmd.append('--fixminer-mode')
             if finish_correct_patch:
                 cmd.append('--finish-correct-patch')
-            if timeout>0:
-                cmd.append('-T')
-                cmd.append(str(timeout))
-            if max_iter>0:
-                cmd.append('-E')
-                cmd.append(str(max_iter))
-            result=subprocess.run(cmd+['--','python3','../script/d4j_run_test.py',project_path])
-    
+            execute_cmd(cmd+['--','python3','../script/d4j_run_test.py',project_path])
+        else: # recoder, alpharepair
+            project = f"{proj}-{bid}"
+            cmd = default_opts + ["-o", output_dir, "-m", "guided", "--recoder-mode",
+                    '-w',f'{tool}/d4j/{project}']
+            if project in correct_dict:
+                cmd.append('-c')
+                cmd.append(correct_dict[project])
+            if finish_correct_patch:
+                cmd.append('--finish-correct-patch')
+            execute_cmd(cmd+['--','python3','../script/d4j_run_test.py',project_path])
+            
     elif mode=='original':
         if tool=='avatar' or tool=='fixminer' or tool=='kpar' or tool=='tbar':
-            cmd=['python3','../casino.py','-o',output_dir,'-m','tbar',"--use-exp-alpha",
-                    '--tbar-mode','-w',f'{tool}/d4j/{project}/0' if tool=='fixminer' else f'{tool}/d4j/{project}','-t','180000','--use-pass-test']
+            project = f"{proj}_{bid}"
+            cmd = default_opts + ["-o", output_dir, "-m", "tbar", "--tbar-mode",
+                    '-w',f'{tool}/d4j/{project}/0' if tool=='fixminer' else f'{tool}/d4j/{project}']
             if project in correct_dict:
                 cmd.append('-c')
                 cmd.append(correct_dict[project])
@@ -81,18 +96,23 @@ def main(argv):
                 cmd.append('--fixminer-mode')
             if finish_correct_patch:
                 cmd.append('--finish-correct-patch')
-            if timeout>0:
-                cmd.append('-T')
-                cmd.append(str(timeout))
-            if max_iter>0:
-                cmd.append('-E')
-                cmd.append(str(max_iter))
-            result=subprocess.run(cmd+['--','python3','../script/d4j_run_test.py',project_path])
-
+            execute_cmd(cmd+['--','python3','../script/d4j_run_test.py',project_path])
+        else: # recoder, alpharepair
+            project = f"{proj}-{bid}"
+            cmd = default_opts + ["-o", output_dir, "-m", "recoder", "--recoder-mode",
+                    '-w',f'{tool}/d4j/{project}']
+            if project in correct_dict:
+                cmd.append('-c')
+                cmd.append(correct_dict[project])
+            if finish_correct_patch:
+                cmd.append('--finish-correct-patch')
+            execute_cmd(cmd+['--','python3','../script/d4j_run_test.py',project_path])
+            
     elif mode=='seapr':
         if tool=='avatar' or tool=='fixminer' or tool=='kpar' or tool=='tbar':
-            cmd=['python3','../casino.py','-o',output_dir,'-m','seapr',"--use-exp-alpha",'--use-pattern','--ignore-compile-error',
-                    '--tbar-mode','-w',f'{tool}/d4j/{project}/0' if tool=='fixminer' else f'{tool}/d4j/{project}','-t','180000','--use-pass-test']
+            project = f"{proj}_{bid}"
+            cmd = default_opts + ["-o", output_dir, "-m", "seapr", "--tbar-mode", "--ignore-compile-error", "--use-pattern",
+                    '-w',f'{tool}/d4j/{project}/0' if tool=='fixminer' else f'{tool}/d4j/{project}']
             if project in correct_dict:
                 cmd.append('-c')
                 cmd.append(correct_dict[project])
@@ -100,10 +120,9 @@ def main(argv):
                 cmd.append('--fixminer-mode')
             if finish_correct_patch:
                 cmd.append('--finish-correct-patch')
-            if timeout>0:
-                cmd.append('-T')
-                cmd.append(str(timeout))
-            if max_iter>0:
-                cmd.append('-E')
-                cmd.append(str(max_iter))
-            result=subprocess.run(cmd+['--','python3','../script/d4j_run_test.py',project_path])
+            execute_cmd(cmd+['--','python3','../script/d4j_run_test.py',project_path])
+        else: # recoder, alpharepair
+            project = f"{proj}-{bid}"
+            cmd = default_opts + ["-o", output_dir, "-m", "seapr", "--recoder-mode", 
+                                  "--ignore-compile-error", '-w',f'{tool}/d4j/{project}']
+            execute_cmd(cmd+['--','python3','../script/d4j_run_test.py',project_path])
