5b6992b4dee7a4a266a78b43f0c795d2b22f8be3
FreddyYJ <kyj1411@unist.ac.kr>
2023-02-16 10:42:51 +0900

    Update repo

--- experiment/tbar/TBarFixRunner.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bugDataPath=$1
-bugID=$2
-defects4jHome=$3
-out=$4
-
-java -Xmx100g -Xss500M -cp "target/dependency/*" edu.lu.uni.serval.tbar.main.MainPerfectFLPatchSpace $bugDataPath $bugID $defects4jHome $out
